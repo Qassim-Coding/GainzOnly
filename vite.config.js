a7baf9d75fe32e73866e1114f8d3a0b2817d90c3
Qassim Osseni <qassimosseni@gmail.com>
2025-09-08 01:24:24 +0200

    ajout config github pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // IMPORTANT pour GitHub Pages: remplace par le nom exact du repo
+  base: '/GainzOnly/',
   plugins: [react(), tailwindcss()],
 })
